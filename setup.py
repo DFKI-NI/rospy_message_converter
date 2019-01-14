a01b0c56ffa0446761c4c0c7d0357fda027b7c2f
Martin Günther <martin.guenther@dfki.de>
2019-01-14 11:08:44 +0100

    Add warning comment to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
