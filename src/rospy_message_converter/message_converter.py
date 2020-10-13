DFKI-NI/rospy_message_converter

commit 35f12383a1652792dd2fdf73e8b2e19b9da92fc9

Avoid numpy dependency

src/rospy_message_converter/message_converter.py
@@ -36,7 +36,7 @@ import rospy
 import base64
 import sys
 import copy
-import numpy as np
+import collections
 
 python3 = (sys.hexversion > 0x03000000)
 
@@ -53,21 +53,47 @@ python_float_types = [float]
 
 ros_to_python_type_map = {
     'bool'    : [bool],
-    'float32' : python_float_types + python_int_types + [np.float32, np.int8, np.int16, np.uint8, np.uint16],
-       # don't include int32, because conversion to float may change value: v = np.iinfo(np.int32).max; np.float32(v) != v
-    'float64' : python_float_types + python_int_types + [np.float32, np.float64, np.int8, np.int16, np.int32, np.uint8, np.uint16, np.uint32],
-    'int8'    : python_int_types + [np.int8],
-    'int16'   : python_int_types + [np.int8, np.int16, np.uint8],
-    'int32'   : python_int_types + [np.int8, np.int16, np.int32, np.uint8, np.uint16],
-    'int64'   : python_int_types + [np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32],
-    'uint8'   : python_int_types + [np.uint8],
-    'uint16'  : python_int_types + [np.uint8, np.uint16],
-    'uint32'  : python_int_types + [np.uint8, np.uint16, np.uint32],
-    'uint64'  : python_int_types + [np.uint8, np.uint16, np.uint32, np.uint64],
-    'byte'    : python_int_types + [np.int8],
-    'char'    : python_int_types + [np.uint8],
+    'float32' : python_float_types + python_int_types,
+    'float64' : python_float_types + python_int_types,
+    'int8'    : python_int_types,
+    'int16'   : python_int_types,
+    'int32'   : python_int_types,
+    'int64'   : python_int_types,
+    'uint8'   : python_int_types,
+    'uint16'  : python_int_types,
+    'uint32'  : python_int_types,
+    'uint64'  : python_int_types,
+    'byte'    : python_int_types,
+    'char'    : python_int_types,
     'string'  : python_string_types
 }
+
+try:
+    import numpy as np
+    _ros_to_numpy_type_map = {
+        'float32' : [np.float32, np.int8, np.int16, np.uint8, np.uint16],
+        # don't include int32, because conversion to float may change value: v = np.iinfo(np.int32).max; np.float32(v) != v
+        'float64' : [np.float32, np.float64, np.int8, np.int16, np.int32, np.uint8, np.uint16, np.uint32],
+        'int8'    : [np.int8],
+        'int16'   : [np.int8, np.int16, np.uint8],
+        'int32'   : [np.int8, np.int16, np.int32, np.uint8, np.uint16],
+        'int64'   : [np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32],
+        'uint8'   : [np.uint8],
+        'uint16'  : [np.uint8, np.uint16],
+        'uint32'  : [np.uint8, np.uint16, np.uint32],
+        'uint64'  : [np.uint8, np.uint16, np.uint32, np.uint64],
+        'byte'    : [np.int8],
+        'char'    : [np.uint8],
+    }
+
+    # merge type_maps
+    merged = collections.defaultdict(list, ros_to_python_type_map)
+    for k, v in _ros_to_numpy_type_map.items():
+        merged[k].extend(v)
+    ros_to_python_type_map = dict(merged)
+except ImportError:
+    pass
+
 
 ros_time_types = ['time', 'duration']
 ros_primitive_types = ['bool', 'byte', 'char', 'int8', 'uint8', 'int16',

package.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0"?>
-<package>
+<package format="3">
   <name>rospy_message_converter</name>
   <version>0.5.3</version>
   <description>Converts between Python dictionaries and JSON to rospy messages.</description>
@@ -15,11 +15,13 @@
   <build_depend>message_generation</build_depend>
   <build_depend>std_msgs</build_depend>
 
-  <run_depend>roslib</run_depend>
-  <run_depend>rospy</run_depend>
-  <run_depend>message_runtime</run_depend>
-  <run_depend>std_msgs</run_depend>
+  <exec_depend>roslib</exec_depend>
+  <exec_depend>rospy</exec_depend>
+  <exec_depend>message_runtime</exec_depend>
+  <exec_depend>std_msgs</exec_depend>
 
   <test_depend>rosunit</test_depend>
   <test_depend>std_srvs</test_depend>
+  <test_depend condition="$ROS_PYTHON_VERSION == 2">python-numpy</test_depend>
+  <test_depend condition="$ROS_PYTHON_VERSION == 3">python3-numpy</test_depend>
 </package>

commit 61219728de00c64e806035c1f381083b0202067b

Deepcopy type lists
This fixes a bug introduced in 35f12383a and caught by
test_dictionary_with_numpy_conversions: Without the deepcopy,
python_int_types is mutated by merging the dictionaries, which leads to
all int entries in the ros_to_python_type_map dictionary being the same.

src/rospy_message_converter/message_converter.py
@@ -53,19 +53,19 @@ python_float_types = [float]
 
 ros_to_python_type_map = {
     'bool'    : [bool],
-    'float32' : python_float_types + python_int_types,
-    'float64' : python_float_types + python_int_types,
-    'int8'    : python_int_types,
-    'int16'   : python_int_types,
-    'int32'   : python_int_types,
-    'int64'   : python_int_types,
-    'uint8'   : python_int_types,
-    'uint16'  : python_int_types,
-    'uint32'  : python_int_types,
-    'uint64'  : python_int_types,
-    'byte'    : python_int_types,
-    'char'    : python_int_types,
-    'string'  : python_string_types
+    'float32' : copy.deepcopy(python_float_types + python_int_types),
+    'float64' : copy.deepcopy(python_float_types + python_int_types),
+    'int8'    : copy.deepcopy(python_int_types),
+    'int16'   : copy.deepcopy(python_int_types),
+    'int32'   : copy.deepcopy(python_int_types),
+    'int64'   : copy.deepcopy(python_int_types),
+    'uint8'   : copy.deepcopy(python_int_types),
+    'uint16'  : copy.deepcopy(python_int_types),
+    'uint32'  : copy.deepcopy(python_int_types),
+    'uint64'  : copy.deepcopy(python_int_types),
+    'byte'    : copy.deepcopy(python_int_types),
+    'char'    : copy.deepcopy(python_int_types),
+    'string'  : copy.deepcopy(python_string_types)
 }
 
 try: