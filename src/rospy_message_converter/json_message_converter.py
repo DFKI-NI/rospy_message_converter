f4bb2ad7cff6ec23ee9fff5c6ac151dbeef5a763
Otacon5555 <michele.ragazzo@external.thalesgroup.com>
2020-11-09 17:07:58 +0100

    Add param binary_array_as_bytes
    Closes #45.

--- src/rospy_message_converter/json_message_converter.py
+++ src/rospy_message_converter/json_message_converter.py
@@ -16,7 +16,7 @@ def convert_json_to_ros_message(message_type, json_message, strict_mode=True):
     dictionary = json.loads(json_message)
     return message_converter.convert_dictionary_to_ros_message(message_type, dictionary, strict_mode=strict_mode)
 
-def convert_ros_message_to_json(message):
+def convert_ros_message_to_json(message, binary_array_as_bytes=True):
     """
     Takes in a ROS message and returns a JSON-formatted string.
 
@@ -24,6 +24,6 @@ def convert_ros_message_to_json(message):
         ros_message = std_msgs.msg.String(data="Hello, Robot")
         json_message = convert_ros_message_to_json(ros_message)
     """
-    dictionary = message_converter.convert_ros_message_to_dictionary(message)
+    dictionary = message_converter.convert_ros_message_to_dictionary(message, binary_array_as_bytes)
     json_message = json.dumps(dictionary)
     return json_message
